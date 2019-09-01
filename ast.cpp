a223fb474ec4323cc3508ae1db6a1309d4e36f87
maoliangxian <sacrex@163.com>
2019-09-01 21:42:58 +0800

    v0.1

--- ast.cpp
+++ ast.cpp
@@ -70,8 +70,8 @@ class FunctionAST
 	std::unique_ptr<PrototypeAST> Proto;
 	std::unique_ptr<ExprAST> Body;
 public:
-	FunctionAST(std::unqiue_ptr<PrototypeAST> Proto,
-					std::unqiue_ptr<ExprAST> Body)
+	FunctionAST(std::unique_ptr<PrototypeAST> Proto,
+					std::unique_ptr<ExprAST> Body)
 			:Proto(std::move(Proto)), Body(std::move(Body)) {}
 };
 }
---
+++ toy.cpp
@@ -0,0 +1,15 @@
+#include "llvm-3.8/llvm/ADT/STLExtras.h"
+#include <algorithm>
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "lexer.cpp"
+
+#include "ast.cpp"
+
+#include "parser.cpp"
